b212f87762b7fd1601740397fb885b28e322df82
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-16 13:49:44 +0100

    Adding positive and negative tests for the get_item endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,3 +12,14 @@ def test_healthy_check_mulfunctioning():
     response = client.get("/wrong-url")
     assert response.status_code == 404
     assert response.json() == {'detail': 'Not Found'}
+
+def test_get_item_valid():
+    response = client.get("/items/1")
+    response.status_code == 200
+    response.json() == 4
+
+def test_get_item_invalid():
+    response = client.get("/items/1000")
+    response.status_code == 404
+    response.json() == {'detail': 'Item not in the database.'}
+
