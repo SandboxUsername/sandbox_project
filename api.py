594d3fd698923d63f18912381e320d5793c05b8c
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-16 13:48:53 +0100

    Handling wrong item's ids

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -10,5 +10,7 @@ items = {str(x + 1): x * 7 for x in range(21)}
 
 @app.get("/items/{id}")
 def get_item(id):
+    if id not in items:
+        raise HTTPException(status_code=404, detail='Item not in the database.')
     item = items[id]
     return item
