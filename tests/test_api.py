e352057d8e2d4b1cae78108a2004223a07fb4229
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-16 13:23:59 +0100

    Adding a negative test to the healthy_check

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,3 +7,8 @@ def test_healthy_check():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'Working': True}
+
+def test_healthy_check_mulfunctioning():
+    response = client.get("/wrong-url")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Not Found'}
