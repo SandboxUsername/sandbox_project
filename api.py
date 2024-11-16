476c37f3942998d5a4a83aae20c86725ead097ea
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-16 13:38:24 +0100

    Adding endpoint to get an item

--- api.py
+++ api.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def health_check():
     return {'Working': True}
+
+items = {str(x + 1): x * 7 for x in range(21)}
+
+@app.get("/items/{id}")
+def get_item(id):
+    item = items[id]
+    return item
