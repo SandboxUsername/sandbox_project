67da34112cc74d1eec7400c0fa358cada4de3101
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-16 17:47:25 +0100

    Items ids must be integers and have enforcing typhinting

--- api.py
+++ api.py
@@ -6,10 +6,10 @@ app = FastAPI()
 def health_check():
     return {'Working': True}
 
-items = {str(x + 1): x * 7 for x in range(21)}
+items = {x + 1: x * 7 for x in range(21)}
 
 @app.get("/items/{id}")
-def get_item(id):
+def get_item(id: int):
     if id not in items:
         raise HTTPException(status_code=404, detail='Item not in the database.')
     item = items[id]
