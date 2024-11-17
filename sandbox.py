a5b6d0218c418cf2cd273774e7a62102a6e4f84b
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-17 16:51:45 +0100

    Understanding fixtures from pytest

--- sandbox.py
+++ sandbox.py
@@ -1,27 +1,13 @@
-from fastapi.testclient import TestClient
-from fastapi import Depends, FastAPI, HTTPException
+import pytest
 
-app = FastAPI()
-client = TestClient(app=app)
+@pytest.fixture
+def a():
+    print(1)
+    yield
+    print(2)
 
-DDBB = {x + 1: str(x * 7) for x in range(21)}
+def x():
+    return 123
 
-def get_database():
-    return DDBB
-
-@app.get("/items/{id}")
-def get_item(id: int, db=Depends(get_database)):
-    if id not in db:
-        raise HTTPException(status_code=404, detail='Item not in the database.')
-    item = db[id]
-    return item
-
-def mock_get_database():
-    return {1: '2'}
-
-def test_get_item_valid_mock():
-    app.dependency_overrides[get_database] = mock_get_database
-    print(app.dependency_overrides)
-    response = client.get('/items/1')
-    assert response.status_code == 200
-    assert response.json() == "2"
+def test_x(a):
+    assert x() == 123
