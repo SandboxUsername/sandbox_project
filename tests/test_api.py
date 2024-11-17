3ed4446523d1b0b4b3897c68825cba19e9edf300
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-17 22:49:32 +0100

    TDD created positive test for get_items endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,25 @@
 from fastapi.testclient import TestClient
-from api import app
+from api import app, get_database
+import pytest
+from database_management import Item
 
 client = TestClient(app=app)
+
+def mock_get_database():
+    return [Item(id=1, description='First item'), Item(id=2, description='Second item')]
+
+@pytest.fixture
+def override_db_dependency():
+    app.dependency_overrides[get_database] = mock_get_database
+    yield
+    app.dependency_overrides.clear()
 
 def test_healthy_check():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'Working': True}
+
+def test_get_items():
+    response = client.get("/items")
+    assert response.status_code == 200
+    assert response.json() == [Item(id=1, description='First item'), Item(id=2, description='Second item')]
