d9c9ab8cb9a1e431be9de4089a5a2e784858744b
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-17 11:40:32 +0100

    Inverting the dependency related to the DDBB

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from schemas import Item
 
 app = FastAPI()
@@ -7,20 +7,25 @@ app = FastAPI()
 def health_check():
     return {'Working': True}
 
-items = {x + 1: str(x * 7) for x in range(21)}
+DDBB = {x + 1: str(x * 7) for x in range(21)}
+
+def get_database():
+    return DDBB
 
 @app.get("/items/{id}")
-def get_item(id: int):
-    if id not in items:
+def get_item(id: int, db=Depends(get_database)):
+    if id not in db:
         raise HTTPException(status_code=404, detail='Item not in the database.')
-    item = items[id]
+    item = db[id]
     return item
 
 @app.post("/items")
-def create_item(item: Item):
-    if item.id - 1 != int(list(items.keys())[-1]):
+def create_item(item: Item, db=Depends(get_database)):
+    if item.id - 1 != int(list(db.keys())[-1]):
         raise HTTPException(status_code=400, detail="Wrong id.")
     if item.id - 1 != int(item.description) / 7:
         raise HTTPException(status_code=400, detail="Wrong description.")
-    items[item.id] = item.description
+    # breakpoint()
+    db[item.id] = item.description
+    # breakpoint()
     return item
