cc5f5e5dc81bc466a16b208e68c888464c6b4d50
Boria Jimenez <cboriaj@emea.corpdir.net>
2024-11-18 01:37:50 +0100

    Created endpoint for creation of item

--- api.py
+++ api.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlmodel import Session, select
+
+from database_management import Item, get_database
 
 app = FastAPI()
 
 @app.get("/")
 def healthy_check():
     return {'Working': True}
+
+@app.get("/items")
+def get_items(session: Session = Depends(get_database)):
+    return session.exec(select(Item)).all()
+
+@app.post("/item")
+def create_item(item: Item, session: Session = Depends(get_database)):
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+    return item
